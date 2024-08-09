54ae8678209657e88ef94399f81d311b86b2f2c3
shonilbhide <bhideshonil@gmail.com>
2024-08-08 20:59:18 -0400

    making changes

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
-const path = require('path')
- 
 module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
-      },
-    ],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 }
