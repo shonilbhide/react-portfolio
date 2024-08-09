8e506485fde3965a9096776014bca3d649bc83c7
shonilbhide <bhideshonil@gmail.com>
2024-08-08 21:15:44 -0400

    installing packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
+// next.config.js
 module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  distDir: 'build',
 }
