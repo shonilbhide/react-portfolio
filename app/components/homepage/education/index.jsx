8a68cd67c376be7dfbb7d610ef6deb5923459bf9
shonilbhide <bhideshonil@gmail.com>
2024-08-29 17:02:30 -0400

    changing background, skills, image, adding a experience section

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -71,6 +71,7 @@ function Education() {
           <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent  w-full" />
         </div>
       </div>
+      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl opacity-20"></div>
 
       <div className="flex justify-center my-5 lg:py-8">
         <div className="flex  items-center">
--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -10,6 +10,13 @@ import GlowCard from "../../helper/glow-card";
 function Experience() {
   return (
     <div id="experience" className="relative z-50 border-t border-b my-12 lg:my-24 border-[#25213b]">
+      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl opacity-20"></div>
+
+      <div className="flex justify-center -translate-y-[1px]">
+        <div className="w-3/4">
+          <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent w-full" />
+        </div>
+      </div>
       <div className="flex justify-center my-5 lg:py-8">
         <div className="flex items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
@@ -20,8 +27,9 @@ function Experience() {
         </div>
       </div>
 
-      <div className="py-8 flex flex-col">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
+
+      <div className="py-8 flex ">
+        <div className="grid grid-cols-1 lg:grid-cols-1 lg:col-span-2 gap-8 lg:gap-8">
           <div className='cube-section justify-center w-full h-full flex items-center gap-x-12 px-12 py-12'>
             <div className="cubespinner">
               <div className="face1">
@@ -45,7 +53,7 @@ function Experience() {
             </div>
           </div>
 
-          <div className='card-section' >
+          <div className='card-section lg:grid-cols-6' >
             <div className="flex flex-col gap-6">
               {
                 experiences.map(experience => (
@@ -64,9 +72,6 @@ function Experience() {
                         </p>
                       </div>
                       <div className="flex items-center gap-x-8 px-3 py-5">
-                        <div className="text-violet-500 transition-all duration-300 hover:scale-125">
-                          <BsPersonWorkspace size={36} />
-                        </div>
                         <div>
                           <p className="text-base sm:text-xl mb-2 font-medium uppercase">
                             {experience.title}
