bbfde7d057bc3a40f8d7f27a8d9660daeea4b3fb
shonilbhide <bhideshonil@gmail.com>
2025-05-27 10:27:13 -0400

    Removinf unused files

--- my-portfolio/pages/_app.js
+++ my-portfolio/pages/_app.js
@@ -1,10 +1,8 @@
-import Navbar from "../components/Navbar";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
     </>
   );
